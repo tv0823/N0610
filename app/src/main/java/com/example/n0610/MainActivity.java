b3055ad8079aa94815e12468a0530159e1c3d6c9
tv0823 <tv0823@bs.amalnet.k12.il>
2024-02-26 14:48:51 +0200

    final commit

--- app/src/main/java/com/example/n0610/MainActivity.java
+++ app/src/main/java/com/example/n0610/MainActivity.java
@@ -1,18 +1,36 @@
 package com.example.n0610;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Switch;
 import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
-    Switch SW;
+    Switch sW;
     ToggleButton tB;
-
+    ConstraintLayout cL;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sW = findViewById(R.id.sW);
+        tB = findViewById(R.id.tB);
+        cL = findViewById(R.id.cL);
+    }
+
+    public void pressed(View view) {
+        if(sW.isChecked() && tB.isChecked())
+            cL.setBackgroundColor(Color.BLUE);
+        else if(sW.isChecked() && !tB.isChecked())
+            cL.setBackgroundColor(Color.GREEN);
+        else if(!sW.isChecked() && tB.isChecked())
+            cL.setBackgroundColor(Color.YELLOW);
+        else
+            cL.setBackgroundColor(Color.LTGRAY);
     }
 }
