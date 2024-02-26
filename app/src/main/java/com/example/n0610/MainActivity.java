7752adc578b9c5d04c19e4b3ae3c765315a27c15
tv0823 <tv0823@bs.amalnet.k12.il>
2024-02-26 14:36:45 +0200

    new id's

--- app/src/main/java/com/example/n0610/MainActivity.java
+++ app/src/main/java/com/example/n0610/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.n0610;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Switch;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
+    Switch SW;
+    ToggleButton tB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
